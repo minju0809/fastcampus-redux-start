0449120388b369ae87049da5c1b800e14fea0aeb
minju0809 <loverminju88@naver.com>
2022-10-12 21:08:15 +0900

    ~createStore

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./redux/store";
+import { addTodo } from "./redux/actions";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
+// console.log(store);
+// console.log(store.getState());
+store.dispatch(addTodo("coding"));
+store.dispatch(addTodo("read book"));
+store.dispatch(addTodo("eat"));
+// console.log(store.getState());
+unsubscribe();
+store.dispatch(addTodo("coding"));
+store.dispatch(addTodo("read book"));
+store.dispatch(addTodo("eat"));
+// 콘솔에 표시되지는 않지만 state는 변경 된다.
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
